054796e3706346ac1420532c534c79b34735dd81
Akshay <akshayajay101@gmail.com>
2021-08-06 18:14:28 +0530

    added profile template

--- webpage/presentation.py
+++ webpage/presentation.py
@@ -6,8 +6,15 @@ presentation = Blueprint('presentation',__name__)
 @presentation.route('/home')
 @login_required
 def home():
-    return render_template('home.html',user=current_user)
+    name = current_user.name
+    return render_template('home.html',user=current_user,name=name)
 
 @presentation.route('/')
 def welcome():
     return render_template('welcome.html',user=current_user)
+
+@presentation.route('/profile')
+@login_required
+def profile():
+    name=current_user.name
+    return render_template('profile.html',user=current_user,name=name)
